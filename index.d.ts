8094a4b893514979d9d24c1a0fb3ef6bf8d8c7da
Jason Carter <jason@myharvestlife.com>
2020-10-10 15:59:59 -0400

    fix: index.d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 /**
  * These are the types for the Awaitable Array
  */
-export = class AwaitableArray extends Array {
+declare class AwaitableArray extends Array {
   untilLength(length: number, padding?: number, timeout?: number): Promise<void>;
   untilCondition(condition: () => {}, padding?: number, timeout?: number): Promise<void>;
 }
+
+export = AwaitableArray;
